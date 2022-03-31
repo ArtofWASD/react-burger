c32a51d9f25d68317166ca535497f6e5fd4544c5
Leonid <Chunl3e666@yandex.ru>
2022-03-31 10:52:58 +0300

    refactor : Перенос api и fetch в отлельный файл

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,25 +4,15 @@ import BurgerConstructor from "../burger-constructor/burger-constructor";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import styles from "./app.module.css";
 import { BurgerContext } from "../../services/burger-context";
-import { Ingridients } from '../../utils/api'
+import { getIngridients } from "../../utils/api" 
 
 function App() {
-  const url = `${Ingridients}`;
-  const [state, setState] = useState([null]);
-  console.log(url);
+
+  const [ingridients, setIngridients] = useState(null);
+
   useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        if (response.ok) {return response.json();
-        }
-        throw new Error('Не пришёл ответ от сервера');
-      })
-      .then((data) => {
-        if (data.success) {
-          return setState(data)
-        }
-        throw new Error('Данные не поступили в стейт');
-      })
+    getIngridients()
+      .then(setIngridients)
       .catch((e) => console.error(e));
   }, []);
 
@@ -31,8 +21,8 @@ function App() {
       <header className={styles.header}>
         <AppHeader />
       </header>
-      <BurgerContext.Provider value={state.data}>
-        {state.data && <main className="grid grid-cols-2 gap-16 pt-20 absolute">
+      <BurgerContext.Provider value={ingridients}>
+        {ingridients && <main className="grid grid-cols-2 gap-16 pt-20 absolute">
           <BurgerIngredients/>
           <BurgerConstructor/>
         </main>}
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,2 +1,19 @@
-export const Ingridients = 'https://norma.nomoreparties.space/api/ingredients'
-export const Orders = 'https://norma.nomoreparties.space/api/orders'
+import { API_URL } from "./api-constant";
+
+export const getIngridients = () => {
+
+  return fetch(`${API_URL}/ingredients`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Не пришёл ответ от сервера");
+    })
+    .then((data) => {
+      if (data.success) 
+        return data.data;
+    })    
+};
+
+export const Ingridients = "https://norma.nomoreparties.space/api/ingredients";
+export const Orders = "https://norma.nomoreparties.space/api/orders";
