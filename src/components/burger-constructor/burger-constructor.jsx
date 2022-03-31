f8fc4815881fbdb2e9d6734ee33b85446a27cb1d
Leonid <Chunl3e666@yandex.ru>
2022-03-31 11:06:46 +0300

    refactor: перенос fetch (post) в файл api.jsx

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -4,21 +4,23 @@ import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import { BurgerContext } from "../../services/burger-context";
-import { Orders } from '../../utils/api'
+import { postOrder } from "../../utils/api";
 import Modal from "../modal/modal";
 import OrderDetails from "../order-details/order-details";
 import styles from "./burger-constructor.module.css";
 
 function BurgerConstructor() {
-  const postUrl = `${Orders}`;
   const data = useContext(BurgerContext);
   const [modalActive, setModalActive] = useState(false);
   const [orderId, setOrderId] = useState(null);
   const bun = data[0];
-  const ingredients = data.filter(item => item.type !== 'bun')
+  const ingredients = data.filter((item) => item.type !== "bun");
   const initialState = { totalPrice: 0 };
   // сумма заказа на основе корзины
-  const summTotal = ingredients.reduce((sum, ingredient) => sum + ingredient.price, bun.price *2 );
+  const summTotal = ingredients.reduce(
+    (sum, ingredient) => sum + ingredient.price,
+    bun.price * 2
+  );
 
   function reducer(state, action) {
     switch (action.type) {
@@ -32,7 +34,7 @@ function BurgerConstructor() {
         };
       case "totalPrice":
         return {
-          totalPrice: summTotal
+          totalPrice: summTotal,
         };
       default:
         throw new Error(`Wrong type of action: ${action.type}`);
@@ -52,32 +54,12 @@ function BurgerConstructor() {
   // Пост запрос с ингридиентами
   const order = {
     bun: [],
-    ingredients: ingredients.map(item=>item._id),
+    ingredients: ingredients.map((item) => item._id),
   };
 
   function getOrder() {
-
     setOrderId(undefined);
-
-    fetch(postUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(order),
-    })
-      .then((response) => {
-        if (response.ok) {return response.json();
-        }
-        throw new Error('Не пришёл ответ от сервера');
-      })
-      .then((result) => {
-        if (result.success) {
-           return setOrderId(result.order.number)
-        }
-        throw new Error('Не пришёл номер заказа');
-      })
-      .catch((e) => console.error(e));
+    postOrder(order).then(setOrderId);
   }
 
   return (
@@ -114,10 +96,7 @@ function BurgerConstructor() {
           ))}
         </section>
         {/* Нижняя булка */}
-        <section
-          className="flex items-center py-2 pr-3"
-          key={bun.length}
-        >
+        <section className="flex items-center py-2 pr-3" key={bun.length}>
           <div className="pr-2.5 opacity-0">
             <DragIcon type="primary" />
           </div>
@@ -153,7 +132,7 @@ function BurgerConstructor() {
       {/* Модальное окно с номером заказа */}
       {modalActive && (
         <Modal active={modalActive} setActive={setModalActive}>
-          <OrderDetails orderId={orderId}/>
+          <OrderDetails orderId={orderId} />
         </Modal>
       )}
     </section>
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,6 @@
 import { API_URL } from "./api-constant";
 
 export const getIngridients = () => {
-
   return fetch(`${API_URL}/ingredients`)
     .then((response) => {
       if (response.ok) {
@@ -10,10 +9,30 @@ export const getIngridients = () => {
       throw new Error("Не пришёл ответ от сервера");
     })
     .then((data) => {
-      if (data.success) 
-        return data.data;
-    })    
+      if (data.success) return data.data;
+    });
 };
 
-export const Ingridients = "https://norma.nomoreparties.space/api/ingredients";
+export const postOrder = (order) => {
+  return fetch(`${API_URL}/orders`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(order),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Не пришёл ответ от сервера");
+    })
+    .then((result) => {
+      if (result.success) {
+        return result.order.number;
+      }
+      throw new Error("Не пришёл номер заказа");
+    })
+    .catch((e) => console.error(e));
+};
 export const Orders = "https://norma.nomoreparties.space/api/orders";
