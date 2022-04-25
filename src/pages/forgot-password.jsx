b1c3eb05288d0281d02926e19960ab9574d05d3b
Leonid <Chunl3e666@yandex.ru>
2022-04-25 16:08:00 +0300

    feat: Свёрстаны страницы и редьюсеры

--- src/pages/forgot-password.jsx
+++ src/pages/forgot-password.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
-
+import { Input, Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import AppHeader from "../components/app-header/app-header";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css"
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 export default function ForgotPasswordPage() {
+  const [emailValue, setEmailValue] = useState('')
+  const dispatch = useDispatch();
+
   return (
-    <div>forgot-password</div>
-  )
+    <>
+      <AppHeader />
+      <div className="grid justify-center mt-48">
+      <p className={`${styles.font} text-center text-2xl`}>Восстановление пароля</p>
+      <form action="" className="grid gap-6 justify-items-center mt-6">
+          <Input placeholder="Укажите e-mail" type="email" onChange={(e)=>{setEmailValue(e.target.value)}}/>
+          <Button>
+            <p>Восстановить</p>
+          </Button>
+        </form>
+        <p className={`${styles.font_grey} text-center flex justify-center gap-2 py-4 mt-20`}>
+        Вспомнили пароль ? <Link to="/login" className={styles.font_blue}>Войти</Link>
+      </p>
+      </div>
+    </>
+  );
 }
