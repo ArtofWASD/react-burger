02230517519a4c121444988b725355415a9fb9b6
Leonid <Chunl33666@yandex.ru>
2022-04-28 21:43:48 +0300

    Отправка на ревью

--- src/pages/ingridients.jsx
+++ src/pages/ingridients.jsx
@@ -4,27 +4,47 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { fetchData } from "../services/reducers/get-data";
+import AppHeader from "../components/app-header/app-header";
 
 export default function IngridientsPage() {
   const [modalActive, setModalActive] = useState(true);
+  const [isData, setIsData] = useState(null);
   const { id } = useParams();
   const data = useSelector((state) => state.getData.ingridients);
-  console.log(data);
-  const isLogged = useSelector((state) => state.authData.userData.success);
-  const ingredient = data.find((item) => item._id === id);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const ingredient = data.find((item) => item._id === id);
+
   function close() {
-    setModalActive(false)
-    navigate('/', {replace:true})
+    setModalActive(false);
+    navigate("/", { replace: true });
   }
+  useEffect(() => {
+    dispatch(fetchData());
+    aaa();
+  }, []);
+  function aaa(params) {
+    if (data.length === 0) {
+      setIsData(false);
+    } else {
+      setIsData(true);
+    }
+  }
+
   return (
     <>
-      {data.length > 0 ? (
+      {isData ? (
         <Modal active={modalActive} setActive={close} id={id} title={ingredient.name}>
           <IngredientDetails itemId={id} />
         </Modal>
-      ):(<IngredientDetails itemId={id}/>)}
+      ) : (
+        <div className="flex flex-col justify-center items-center">
+          <AppHeader />
+          <div className=" mt-24 w-2/5 ">
+            <IngredientDetails itemId={id} />
+          </div>
+        </div>
+      )}
     </>
   );
 }
