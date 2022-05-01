c34a4adf095d1eea8a37cb6740fab486229d42f6
Leonid <Chunl33666@yandex.ru>
2022-05-01 13:56:49 +0300

    ...

--- src/components/protected-route/ProtectedRoute.jsx
+++ src/components/protected-route/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 
 const ProtectedGuestRoute = ({ children }) => {
   const location = useLocation();
-  const isUser = useSelector((state) => state.authData.userData);
+  const isUser = useSelector((state) => state.authData.userData.success);
   console.log(isUser);
   if (!isUser) {
     return <Navigate to="/login" state={{ from: location }} />;
