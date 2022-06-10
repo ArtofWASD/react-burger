f35c85920e1158466e86ecfc6e5e33506a1d60f7
Leonid <Chunl3e666@yandex.ru>
2022-06-10 14:32:15 +0300

    feat: тесты для логин-редьюсера

--- src/services/reducers/login-reducer/login.test.ts
+++ src/services/reducers/login-reducer/login.test.ts
@@ -1,28 +1,31 @@
-import reducer, {initialState, logIn, logOut} from './login'
+import reducer, { initialState, logIn, logOut } from "./login";
 
 const loginData = {
-    success: true,
-    accessToken:'accessToken',
-    refreshToken:'refreshToken',
-    user:{
-      email: 'email',
-      name: 'name',
-    },
-  loginState:true
-}
+  success: true,
+  accessToken: "accessToken",
+  refreshToken: "refreshToken",
+  user: {
+    email: "email",
+    name: "name",
+  },
+};
 
-// describe('login', () => {
-//   describe('reducers', () => {
-//     it('reducer fulfilled', () => {
-//       const action = {type: logIn.fulfilled.type, payload:{loginData}};
-//       const state = reducer(initialState, action);
-//       expect(state).toEqual(loginData)
-      
-//     })
-//   })
-// })
+describe("login", () => {
+  test("Set data and loginState when logIn extraReducer is fulfilled", () => {
+    const action = { type: logIn.fulfilled.type, payload: loginData, loginState: true };
+    const state = reducer(initialState, action);
+    expect(state).toEqual({ loginData, loginState: true });
+  });
+});
 
+describe("logOut", () => {
+  test("Set loginState to false when extraReducer logOut is fulfilled", () => {
+    const action = { type: logOut.fulfilled.type, payload: { loginState: false } };
+    const state = reducer(initialState, action);
+    expect(state).toEqual({ ...initialState, loginState: false });
+  });
+});
 
 test("Should return the initial state", () => {
-    expect(reducer(undefined, { type: "" })).toEqual(initialState);
-  });
+  expect(reducer(undefined, { type: "" })).toEqual(initialState);
+});
