1ec785fb5967188ba2c1cdb83162fb227b8a1b18
Leonid <Chunl3e666@yandex.ru>
2022-06-17 17:37:53 +0300

    feat: dev ветка для разработки

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -3,40 +3,48 @@ import { Logo, BurgerIcon, ListIcon, ProfileIcon } from "@ya.praktikum/react-dev
 import { NavLink, Link, useLocation } from "react-router-dom";
 import styles from "./app-header.module.css";
 import { useAppSelector } from "../../utils/hook";
+import { pushRotate as Menu } from "react-burger-menu";
 
 const AppHeader: FC = () => {
   const location = useLocation();
-  const user = useAppSelector(state => state.userData.userData.user);  
+  const user = useAppSelector((state) => state.userData.userData.user);
+
   return (
-    <header className={styles.header}>
-      <nav className="flex flex-auto justify-center">
-        <section className="flex items-center py-4">
-          <section className="flex gap-2 mr-6">
-            <BurgerIcon type={location.pathname === "/" ? "primary" : "secondary"} />
-            <NavLink to="/" className={({ isActive }) => (isActive ? `${styles.headerTextActive}` : `${styles.headerText}`)}>
-              Конструктор
-            </NavLink>
+    <>
+      <header className={styles.header}>
+        <nav className="flex flex-auto justify-center">
+          <section className="flex items-center py-4">
+            <section className="flex gap-2 mr-6">
+              <BurgerIcon type={location.pathname === "/" ? "primary" : "secondary"} />
+              <NavLink to="/" className={({ isActive }) => (isActive ? `${styles.headerTextActive}` : `${styles.headerText}`)}>
+                Конструктор
+              </NavLink>
+            </section>
+            <section className="flex gap-2">
+              <ListIcon type={location.pathname === "/feed" ? "primary" : "secondary"} />
+              <NavLink to="/feed" className={({ isActive }) => (isActive ? `${styles.headerTextActive}` : `${styles.headerText}`)}>
+                Лента заказов
+              </NavLink>
+            </section>
+            <section className="flex ml-56 mr-80">
+              <Link to="/">
+                <Logo />
+              </Link>
+            </section>
+            <section className="flex gap-2 mr-10">
+              <ProfileIcon type={location.pathname === "/profile" ? "primary" : "secondary"} />
+              <NavLink
+                to="/profile"
+                className={({ isActive }) => (isActive ? `${styles.headerTextActive}` : `${styles.headerText}`)}
+                state={{ from: location }}
+              >
+                {user.name !== undefined ? user.name : "Личный кабинет"}
+              </NavLink>
+            </section>
           </section>
-          <section className="flex gap-2">
-            <ListIcon type={location.pathname === "/feed" ? "primary" : "secondary"} />
-            <NavLink to="/feed" className={({ isActive }) => (isActive ? `${styles.headerTextActive}` : `${styles.headerText}`)}>
-              Лента заказов
-            </NavLink>
-          </section>
-          <section className="flex ml-56 mr-80">
-            <Link to="/">
-              <Logo />
-            </Link>
-          </section>
-          <section className="flex gap-2 mr-10">
-            <ProfileIcon type={location.pathname === "/profile" ? "primary" : "secondary"} />
-            <NavLink to="/profile" className={({ isActive }) => (isActive ? `${styles.headerTextActive}` : `${styles.headerText}`)} state={{ from: location }}>
-              {user.name !== undefined ? (user.name):("Личный кабинет")}
-            </NavLink>
-          </section>
-        </section>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </>
   );
 };
 export default AppHeader;
